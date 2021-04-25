e8dd7ab03898c3d0a1d5167a0d76ce0d87b3fd83
Richard Hofmänner <hello@terillos.dev>
2021-04-25 13:05:41 +0200

    Finished Home site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import HomeLayout from '@/layouts/HomeLayout'
 import Banner from '@/components/Banner'
+import Link from 'next/link'
 
 const React = require('react')
 
@@ -7,8 +8,13 @@ function HomePage() {
   return (
     <HomeLayout title="Welcome">
       <Banner image="/images/bg-home.jpg">
-        <h1 className="mb-40">Linktr.ee</h1>
+        <h1 className="mb-5">Linktr.ee</h1>
         <p className="h2">The simple clone</p>
+        <Link href='/login'>
+          <a className='mt-20 px-5 py-2 border inline-block transition-colors hover:bg-white hover:text-black'>
+            Sign in
+          </a>
+        </Link>
       </Banner>
     </HomeLayout>
   )
